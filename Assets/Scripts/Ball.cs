8a0429ad9fddb1c0fbbe212fa3490de27f942d8c
yorksensei <yorksensei@gmail.com>
2018-01-29 19:15:56 +0900

    Created the game scene, blocks, paddle and ball.

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+	public Paddle paddle;
+
+	private Vector3 paddleToBallVector;
+
+	private bool hasStarted = false;
+
+	// Use this for initialization
+	void Start () {
+		this.GetComponents<Rigidbody2D> ();
+		paddleToBallVector = this.transform.position - paddle.transform.position;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (!hasStarted) {
+			this.transform.position = paddle.transform.position + paddleToBallVector;
+		}	 
+
+		if (Input.GetMouseButtonDown (0)) {
+			hasStarted = true;
+			gameObject.GetComponent<Rigidbody2D> ().velocity = new Vector2 (2f, 10f);
+		}
+	}
+}
---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+
+	float mousePosInBlocks = 0;
+	Vector3 paddlePos = new Vector3(0.5f,0.5f,0f);
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		mousePosInBlocks = (Input.mousePosition.x / Screen.width * 16);
+
+		/*
+		if (paddlePos.x < 0.5f) {
+			paddlePos.x = 0.5f;
+		} else if (paddlePos.x > 15.5f) {
+			paddlePos.x = 15.5f;
+		} */
+
+		paddlePos.x = Mathf.Clamp (mousePosInBlocks, 0.5f, 15.5f);
+
+		this.transform.position = paddlePos;
+	}
+}
